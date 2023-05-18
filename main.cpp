4318c928c93693593d1c64d79f74f860a351e590
Snocci <128066678+Snocci@users.noreply.github.com>
2023-05-18 10:49:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "Libreria.h"
+using namespace std;
 int main(){
+  char n;
+    cin >> n;
+    char x=0;
+    x=funzione1(n);
+    if (x==0){
+        cout<<"errore"<<endl;
+    } else
+        cout<<x<<endl;
   return 0;
 }
